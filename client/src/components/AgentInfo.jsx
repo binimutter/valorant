2dd937342be4dbba65db2d50c4212661b15e94a1
Ubin Jung <jungu@utexas.edu>
2021-08-25 01:07:57 -0700

    css fixes

--- client/src/components/AgentInfo.jsx
+++ client/src/components/AgentInfo.jsx
@@ -25,16 +25,8 @@ const AgentInfo = ({ show, handleClose, agent, description, role, util }) => {
         {util ?
           <div className="agent-util">
             <p className="agent-desc">Abilities:{' '}{util[0].displayName}{',  '}{util[1].displayName}{',  '}{util[2].displayName}{',  '}Ultimate{' '}-{' '}{util[3].displayName}</p>
-            {/* <p className="agent-desc">{util[0].displayName}{', '}</p>
-            <p className="agent-desc">{util[1].displayName}{', '}</p>
-            <p className="agent-desc">{util[2].displayName}{', '}</p>
-            <p className="agent-desc">Ultimate{' '}-{' '}{util[3].displayName}</p> */}
           </div>
           : null}
-        {/* <p className="agent-desc">{agent.description}</p>
-        {agent.role.displayName ?
-          <p className="agent-role">Role: {agent.role.displayName}</p>
-        : null} */}
       </div>
     </div>
   )
